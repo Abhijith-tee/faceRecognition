5425256fc730b170102cf1a1f6f69b117b8872a0
Abhijith T <abhijiththayad111@gmail.com>
2020-07-31 22:06:18 +0530

    Added Rank component and configured react-particles-js for styling the background

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import React from 'react';
 import Navigation from './components/navigation/navigation.component';
 import Logo from './components/logo/logo.component';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm.component';
+import Rank from './components/Rank/rank.component';
 
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Particles className='particles'
+        params={particlesOptions}
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*
         <FaceRecognition />*/}
