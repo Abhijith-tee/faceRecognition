79b7e4f77c5a53b186f91d645aa75da40bbf5cf1
Abhijith T <abhijiththayad111@gmail.com>
2020-08-02 23:02:57 +0530

    setting up facerecognition api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import Clarifai from 'clarifai';
+import Particles from 'react-particles-js';
+
 import Navigation from './components/navigation/navigation.component';
 import Logo from './components/logo/logo.component';
 import ImageLinkForm from './components/imageLinkForm/imageLinkForm.component';
 import Rank from './components/Rank/rank.component';
 
 import './App.css';
-import Particles from 'react-particles-js';
+
+const app = new Clarifai.App({
+  apiKey: 'c3d15756d4aa40b48a79bc565bf40584'
+});
 
 const particlesOptions = {
   particles: {
@@ -35,6 +41,15 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b",
+     "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+
+      }
+    );
   }
 
   render() {
